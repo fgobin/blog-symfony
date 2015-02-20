4a049835246c5541465d34a0606603dab45a6c10
fgobin <filip.gobin@hotmail.com>
2015-02-20 15:02:55 +0100

    Finished task 6, added php support to nginx, sample php page added

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
 #installation of web server
-apt-get -y install nginx
+echo "Installing nginx"
+apt-get -y install nginx > /dev/null
 
 #installation of php5
-apt-get -y install php5-fpm
+echo "Installing php5-fpm"
+apt-get -y install php5-fpm > /dev/null
 
-#TODO: copy configuration
+#copy configuration for nginx
+echo "Copying nginx configuration file"
+cp /var/www/vagrant/nginx/default /etc/nginx/sites-enabled/default
+
+#restart nginx server if running
+echo "Restarting nginx"
+nginx -s stop
+nginx
+
