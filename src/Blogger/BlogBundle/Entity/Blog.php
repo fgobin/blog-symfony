b662350cac92f3e3dd92547830d4423f8b5cc5b8
fgobin <filip.gobin@hotmail.com>
2015-02-28 14:04:20 +0100

    Added twig extension, slugify url-s

--- src/Blogger/BlogBundle/Entity/Blog.php
+++ src/Blogger/BlogBundle/Entity/Blog.php
@@ -30,6 +30,10 @@ class Blog
     protected $title;
 
     /**
+     * @ORM\Column(type="string")
+     */
+    protected $slug;
+    /**
      * @ORM\Column(type="string", length=100)
      */
     protected $author;
@@ -100,6 +104,7 @@ class Blog
     public function setTitle($title)
     {
         $this->title = $title;
+        $this->setSlug($this->title);
 
         return $this;
     }
@@ -310,4 +315,55 @@ class Blog
     }
 
 
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     * @return Blog
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $this->slugify($slug);
+
+        return $this;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string 
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    public function slugify($text)
+    {
+        // replace non letter or digits by -
+        $text = preg_replace('#[^\\pL\d]+#u', '-', $text);
+
+        // trim
+        $text = trim($text, '-');
+
+        // transliterate
+        if (function_exists('iconv'))
+        {
+            $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+        }
+
+        // lowercase
+        $text = strtolower($text);
+
+        // remove unwanted characters
+        $text = preg_replace('#[^-\w]+#', '', $text);
+
+        if (empty($text))
+        {
+            return 'n-a';
+        }
+
+        return $text;
+    }
 }
