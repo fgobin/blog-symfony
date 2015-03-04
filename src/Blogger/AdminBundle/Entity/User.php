f9ddc5bc674e74c568a7e87182e4c6c9eb373d62
fgobin <filip.gobin@hotmail.com>
2015-03-04 14:10:14 +0100

    Added auth for admin pages with FOSUserBundle

---
+++ src/Blogger/AdminBundle/Entity/User.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: filip
+ * Date: 04.03.15.
+ * Time: 12:14
+ */
+
+namespace Blogger\AdminBundle\Entity;
+
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
