d69170606d6541a4feaf4f3ba518523c59f37456
fgobin <filip.gobin@hotmail.com>
2015-04-06 20:24:45 +0200

    Added search box to navbar, and changed search query

--- src/Blogger/BlogBundle/Controller/BlogSearchController.php
+++ src/Blogger/BlogBundle/Controller/BlogSearchController.php
@@ -3,11 +3,13 @@
 namespace Blogger\BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class BlogSearchController extends Controller
 {
-    public function searchAction($query)
+    public function searchAction(Request $request)
     {
+        $query = $request->query->get('query');
         $finder = $this->container->get('fos_elastica.index.app.blog');
 
         $results = $finder->search($query);
